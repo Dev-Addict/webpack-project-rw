e8138c0c7d49f0b4a13afbd335d8b8df2df0020f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-27 12:24:38 +0430

    code splitting(split libs from code)

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
-var webpack = require('webpack');
 var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var VENDOR_LIBS = [
+    'faker',
+    'lodash',
+    'redux',
+    'react-redux',
+    'react-dom',
+    'react-input-range',
+    'redux-form',
+    'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        use: ['style-loader', 'css-loader'],
-        test: /\.css$/
-      }
+    entry: {
+        bundle: './src/index.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
     ]
-  }
 };
